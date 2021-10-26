ce146a127e89caf1378c4926c61e9cdf0c5f112e
Lucas DevOps <51280960+LucasDevolps@users.noreply.github.com>
2021-10-26 13:10:29 -0300

    Atualizando Respositório

---
+++ api.js
@@ -0,0 +1,32 @@
+[
+    {
+        "nome": "Bruno",
+        "telefone": "9999-2222",
+        "data" : "2015-04-12T12:53:46.204z",
+        "operadora":{
+            "nome": "Oi",
+            "codigo": 14,
+            "Categoria": "Celular"
+        }
+    },
+    {
+        "nome": "Sandra",
+        "telefone": "9999-3333",
+        "data" : "2015-04-12T12:53:46.204z",
+        "operadora":{
+            "nome": "Vivo",
+            "codigo": 15,
+            "Categoria": "Celular"
+        }
+    },
+    {
+        "nome": "Mariana",
+        "telefone": "9999-3333",
+        "data" : "2015-04-12T12:53:46.204z",
+        "operadora":{
+            "nome": "Vivo",
+            "codigo": 15,
+            "Categoria": "Celular"
+        }
+    },
+]
